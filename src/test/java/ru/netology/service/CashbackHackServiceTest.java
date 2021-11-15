6bbf72bf7913348143815c2f4cdf503bf219b12d
e.fedoseeva <tetstest@list.ru>
2021-11-16 01:20:15 +0300

    code

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,7 +10,7 @@ public class CashbackHackServiceTest {
     public void shouldRemain(){
 
         CashbackHackService cashback = new CashbackHackService();
-        int amount = 750;
+        int amount = 75;
 
         int actual = cashback.remain(amount);
         int expected = 250;
